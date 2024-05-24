edf9ae3bef386809dcfe98731a581d5be3fe9eda
Evgeny Skopin <79599538+scopchik@users.noreply.github.com>
2024-05-24 20:03:24 +0300

    Update BossHealth.cs

--- Assets/Scripts/Bosses/BossHealth.cs
+++ Assets/Scripts/Bosses/BossHealth.cs
@@ -4,37 +4,26 @@ using UnityEngine;
 
 public class BossHealth : Health
 {
-    
-
-
     public void BossTakeDamage(float _damage)
     {
         bossCurrentHealth = Mathf.Clamp(bossCurrentHealth - _damage, 0 , startingHealth);
         if(bossCurrentHealth > 0)
         {
             anim.SetTrigger("hurt");
-
         }
         else
         {
             if(!dead)
             {
                 anim.SetTrigger("die");
-                
                 foreach(Behaviour component in components)
                 {
                     component.enabled = false;
                 }
-
                 dead = true;
-                
             }    
-            
         }
-        
-        
     }
-    
 
     private void Deactivate()
     {
