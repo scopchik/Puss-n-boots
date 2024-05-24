2a6c4ba15ca4e44dfd0c08f6b84912b1682c8bad
Evgeny Skopin <79599538+scopchik@users.noreply.github.com>
2024-05-24 20:10:23 +0300

    Update next.cs

--- Assets/Scripts/LevelChanger/next.cs
+++ Assets/Scripts/LevelChanger/next.cs
@@ -8,13 +8,7 @@ public class next : MonoBehaviour
     public int levelToLoad;
     public float time;
     public float nextLevel;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if(time > nextLevel)
@@ -22,6 +16,5 @@ public class next : MonoBehaviour
             SceneManager.LoadScene(levelToLoad);
         }
         time += Time.deltaTime;
-    }
-    
+    }   
 }
