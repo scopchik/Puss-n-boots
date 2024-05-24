19f3043271621b9ac13ced54713bdd6d06a694c6
Evgeny Skopin <79599538+scopchik@users.noreply.github.com>
2024-05-24 20:06:04 +0300

    Update Enemy.cs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,11 +6,11 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
     [SerializeField] private int damage;
-
+    
     [Header ("Collider parametres")]
     [SerializeField] private float colliderDistance;
     [SerializeField] private BoxCollider2D boxCollider;
-
+    
     [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
@@ -24,6 +24,7 @@ public class Enemy : MonoBehaviour
         anim = GetComponent<Animator>();
         enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
+    
     private void Update()
     {
         cooldownTimer += Time.deltaTime;
@@ -31,11 +32,9 @@ public class Enemy : MonoBehaviour
         {
             if(cooldownTimer >= attackCooldown)
             {
-                //attack
                 cooldownTimer = 0;
                 anim.SetTrigger("meleeAttack");
             }
-            
         }
         if(enemyPatrol != null)
         {
@@ -48,7 +47,6 @@ public class Enemy : MonoBehaviour
         RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center - transform.right * range * transform.localScale.x * colliderDistance, 
         new Vector3 (boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 
         0, Vector2.left, 0, playerLayer);
-
         if(hit.collider != null){
             playerHealth = hit.transform.GetComponent<Health>();
         }
@@ -66,7 +64,6 @@ public class Enemy : MonoBehaviour
     {
         if(PlayerInSight())
         {
-            //damage player
             playerHealth.TakeDamage(damage);
         }
     }
